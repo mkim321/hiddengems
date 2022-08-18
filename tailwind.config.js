ea1fa6e5dc75b725ac5cd99617e292517f6be73d
Misun <mkim321@my.bcit.ca>
2022-08-18 15:43:22 -0700

    static pages
    build
    *index
    *about
    *about sustainability
    *donate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors:{
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      'hg-navy':'#022859',
+      'hg-salmon': '#F2B28D',
+    },
+    fontFamily:{
+      lato: ['Lato', 'sans-serif'],
+    },
+    extend: {
+      screens:{
+        '2xl':'1447px',
+      },
+    },
   },
   plugins: [require("daisyui")],
 }
